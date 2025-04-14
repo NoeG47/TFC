db35ef2caeded4b269970dec99be45a781e2e8b4
leila52 <lailahaddad385@gmail.com>
2025-04-14 11:30:41 +0200

    carpeta frontend con los componentes y todo instalado

---
+++ Frontend/EasyMeal/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>prueba</p>
+
+    </>
+  )
+}
+
+export default App
