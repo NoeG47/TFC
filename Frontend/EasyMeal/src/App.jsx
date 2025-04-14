00d43abf3603abe3ddd5d97f90e78af1c8ae7a4c
Lorraine2507 <lorenanav14@gmail.com>
2025-04-14 12:00:50 +0200

    instalacion de tailwind y funcionando

--- Frontend/EasyMeal/src/App.jsx
+++ Frontend/EasyMeal/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+//IMPORTAR TAILWIND EN INDEX.CSS
 function App() {
 
   return (
     <>
     <p>prueba</p>
+    <h1 class="text-5xl font-bold underline bg-black">
+    Hello world!
+  </h1>
 
     </>
   )
