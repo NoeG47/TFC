35f8dc7913028160554f25200f153d8ab55d5cee
leila52 <lailahaddad385@gmail.com>
2025-04-17 16:16:59 +0200

    proyecto backen

---
+++ Backend/easymeal/src/main/java/com/esaymeal/easymeal/EasymealApplication.java
@@ -0,0 +1,13 @@
+package com.esaymeal.easymeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymealApplication.class, args);
+	}
+
+}
---
+++ Backend/easymeal/src/test/java/com/esaymeal/easymeal/EasymealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esaymeal.easymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
