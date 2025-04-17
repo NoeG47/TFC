76d21e14ac3dedd1b73f13401b16180319bb1aeb
leila52 <lailahaddad385@gmail.com>
2025-04-17 16:48:41 +0200

    otro proyecto con las dependecias correctas

---
+++ Backend/easy_meal/src/main/java/com/easymeal/easy_meal/EasyMealApplication.java
@@ -0,0 +1,13 @@
+package com.easymeal.easy_meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMealApplication.class, args);
+	}
+
+}
---
+++ Backend/easy_meal/src/test/java/com/easymeal/easy_meal/EasyMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymeal.easy_meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
